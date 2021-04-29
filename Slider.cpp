3ddd7838761a418c8e93e4474661881f9e29f513
Shingo Ohtsuka <capriciouswalk@gmail.com>
2021-04-30 07:56:09 +0900

    とりあえず動くもの

--- Slider.cpp
+++ Slider.cpp
@@ -25,7 +25,7 @@ constexpr std::array<std::array<int, 2>, 4> OFS{{
   {-1, 0},
   {0, -1}
 }};
-constexpr char COMMANDS[5] = "RULD";
+constexpr char DIR_COMMANDS[5] = "RDLU";
 
 struct Timer {
   std::chrono::high_resolution_clock::time_point start;
@@ -50,12 +50,30 @@ bool is_out(const Point& p) {
   return is_out(p.r, p.c);
 }
 
+struct Command {
+  Point point;
+  char type;
+  char dir;
+};
+
 struct Field {
   int Z;
   int grid[MAX_N][MAX_N];
   int score;
   std::shared_ptr<Field> parent;
-  std::string prev_command;
+  std::vector<Command> prev_command;
+
+  Field(const std::shared_ptr<Field> parent)
+      : Z(parent->Z), score(parent->score)
+      , parent(parent), prev_command() {
+    std::memcpy(grid, parent->grid, sizeof(grid));
+  }
+  Field(const Field& parent)
+      : Z(parent.Z), score(parent.score)
+      , prev_command() {
+    std::memcpy(grid, parent.grid, sizeof(grid));
+  }
+  Field() {}
 
   int* operator [](const int index) {
     return grid[index];
@@ -86,14 +104,44 @@ struct Item {
   int score;
 };
 
-std::string solve() {
+std::ostream& operator<<(std::ostream& os, const Point& p)
+{
+  os << "(" << p.r << ", " << p.c << ")";
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Item& item)
+{
+  os << "{point: " << Point{item.r, item.c} << ", color: " << item.color << ", score" << item.score << "}";
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Command& cmd)
+{
+  os << cmd.point.r << " " << cmd.point.c << " " << cmd.type << " " << cmd.dir;
+  return os;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+  os << "[";
+  for (size_t i = 0; i < vec.size(); ++i) {
+    if (i) os << ", ";
+    os << vec[i];
+  }
+  os << "]";
+  return os;
+}
+
+std::vector<Command> solve() {
   auto main_queue_compare = [](const Item& l, const Item& r) {
     return l.score < r.score;
   };
   std::priority_queue<Item, std::vector<Item>, decltype(main_queue_compare)> main_queue{main_queue_compare};
   for (int r = 0; r < N; ++r) {
     for (int c = 0; c < N; ++c) {
-      if (field->at(r, c)> 0) {
+      if (field->at(r, c) > 1) {
         Item item{field->at(r, c), r, c};
         struct Node {
           Point p;
@@ -105,7 +153,7 @@ std::string solve() {
         std::priority_queue<Node, std::vector<Node>, decltype(compare)> queue{compare};
         Node init{{r, c}, 0};
         queue.push(init);
-        bool done[MAX_N][MAX_N];
+        bool done[MAX_N][MAX_N] = {0};
         int best = N * N;
         while (!queue.empty()) {
           Node node = queue.top();
@@ -124,6 +172,7 @@ std::string solve() {
               continue;
             }
             Node next{np, node.score + 1};
+            queue.push(next);
           }
         }
         item.score = -(best * (MAX_C + 1) + field->at(r, c));
@@ -131,8 +180,8 @@ std::string solve() {
       }
     }
   }
-  std::shared_ptr<Field> best = field;
 
+  std::shared_ptr<Field> best = field;
   for (;;) {
     if (main_queue.empty()) {
       break;
@@ -149,11 +198,12 @@ std::string solve() {
       struct Node {
         Point p;
         int prev;
+        char type;
       };
       std::queue<Node> queue;
       int memo[MAX_N][MAX_N];
       std::memset(memo, -1, sizeof(memo));
-      queue.push({{item.r, item.c}, OFS.size()});
+      queue.push({{item.r, item.c}, OFS.size(), '\0'});
       Point hole{-1, -1};
       while(!queue.empty()) {
         Node node = queue.front();
@@ -161,7 +211,7 @@ std::string solve() {
         if (memo[node.p.r][node.p.c] != -1) {
           continue;
         }
-        memo[node.p.r][node.p.c] = node.prev;
+        memo[node.p.r][node.p.c] = node.prev * 256 + node.type;
         if (best->at(node.p.r, node.p.c) == -1) {
           hole = node.p;
           break;
@@ -171,30 +221,32 @@ std::string solve() {
           if (is_out(np)) {
             continue;
           }
-          if (best->at(np.r, np.c) != 0) {
+          if (best->at(np.r, np.c) > 0) {
             continue;
           }
-          queue.push({np, index});
+          queue.push({np, index, 'M'});
         }
       }
       if (hole.r == -1) {
         negatives.push_back(item);
         continue;
       }
-      std::stringstream ss;
+      std::vector<Command> command;
       Point ite = hole;
-      while (memo[ite.r][ite.c] != OFS.size()) {
-        const int index = memo[ite.r][ite.c];
-        ss << COMMANDS[index];
+      while (memo[ite.r][ite.c] != OFS.size() * 256) {
+        const int val = memo[ite.r][ite.c];
+        const int index = val / 256;
+        const char type = val % 256;
         ite.r -= OFS[index][1];
         ite.c -= OFS[index][0];
+        command.push_back({ite, type, DIR_COMMANDS[index]});
       }
-      std::shared_ptr<Field> next = std::make_shared<Field>(*best);
+      std::shared_ptr<Field> next = std::make_shared<Field>(best);
       next->parent = best;
       next->at(item.r, item.c) = 0;
-      next->prev_command = ss.str();
+      next->prev_command = command;
       next->Z -= next->prev_command.size();
-      next->score += std::max(0, next->Z * (item.color - 1));
+      next->score += std::max(0, (next->Z + 1) * (item.color - 1));
       best = next;
     }
 
@@ -204,14 +256,17 @@ std::string solve() {
   }
 
   std::cerr << "Score: " << best->score << std::endl;
-  std::stringstream ss;
+  std::vector<Command> ans;
   std::shared_ptr<Field> ite = best;
   while (ite->parent) {
-    ss << ite->prev_command;
+    ans.insert(ans.end(), ite->prev_command.begin(), ite->prev_command.end());
     ite = ite->parent;
   }
-  std::string ans = ss.str();
   std::reverse(ans.begin(), ans.end());
+
+  if (ans.size() > N * N) {
+    ans.resize(N * N);
+  }
 
   return ans;
 }
@@ -233,7 +288,10 @@ int main() {
 
   const auto ans = solve();
 
-  std::cout << ans << std::endl;
+  std::cout << ans.size() << std::endl;
+  for (const auto& c : ans) {
+    std::cout << c << std::endl;
+  }
 
   std::cout.flush();
 }
