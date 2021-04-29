998af80575a1751d5d865e86cae291b4d8669545
Shingo Ohtsuka <capriciouswalk@gmail.com>
2021-04-29 15:00:59 +0900

    Format

--- Slider.cpp
+++ Slider.cpp
@@ -3,15 +3,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <map>
-#include <sstream>
-#include <vector>
 #include <set>
+#include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main() 
-{
+int main() {
   int N;
   int C;
   int H;
@@ -19,19 +18,19 @@ int main()
   cin >> N;
   cin >> C;
   cin >> H;
-  vector<int> grid(N*N);
-  //read grid
+  vector<int> grid(N * N);
+  // read grid
   int moves = 0;
-  for (int k=0; k<N*N; k++) 
-  {
+  for (int k = 0; k < N * N; k++) {
     cin >> grid[k];
-    if (grid[k]>0) moves++;
+    if (grid[k] > 0)
+      moves++;
   }
   cout << moves << endl;
 
-  for (int r=0; r<N; r++)
-    for (int c=0; c<N; c++)
-      if (grid[r*N+c]>0)
+  for (int r = 0; r < N; r++)
+    for (int c = 0; c < N; c++)
+      if (grid[r * N + c] > 0)
         cout << r << " " << c << " S L" << endl;
 
   cout.flush();
